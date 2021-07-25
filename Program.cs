80aa1270677d878c93c83ad56b0a16ae4522d3dd
Avgust Zagorodnikov <avgust@x-infinity.com>
2021-07-25 13:54:55 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var fieldWidth = 40;
+        var fieldHeight = 30;
+        var field = new bool[fieldWidth, fieldHeight];
+        field[4,5] = true;
+
+        for (int row = 0; row < fieldHeight; row++)
+        {
+            for (int column = 0; column < fieldWidth; column++)
+            {
+                if (field[column,row])
+                {
+                    Console.Write('*');
+                } 
+                else
+                {
+                    Console.Write('.');
+                }  
+            }
+            Console.WriteLine();    
+        }
+        
+        Console.ReadKey();
+    }
+}
