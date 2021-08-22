d3dccb63f1be66a6cc91ff08084692e49b7d4b7a
Avgust Zagorodnikov <avgust@x-infinity.com>
2021-08-22 19:39:05 +0300

    Update

--- Program.cs
+++ Program.cs
@@ -4,9 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        var fieldWidth = 40;
-        var fieldHeight = 30;
-        var field = new bool[fieldWidth, fieldHeight];
+        const int FieldWidth = 100;
+        const int FieldHeight = 50;
+        var field = new bool[FieldWidth, FieldHeight];
         field[4, 5] = true;
         var x = 6;
         var y = 1;
@@ -14,10 +14,9 @@ class Program
         {
             Console.CursorLeft = 0;
             Console.CursorTop = 0;
-            for (int row = 0; row < fieldHeight; row++)
+            for (int row = 0; row < FieldHeight; row++)
             {
-
-                for (int column = 0; column < fieldWidth; column++)
+                for (int column = 0; column < FieldWidth; column++)
                 {
                     if (field[column, row])
                     {
@@ -42,21 +41,91 @@ class Program
                     break;
 
                 case ConsoleKey.RightArrow:
-                    if (x < fieldWidth - 1)
+                    if (x < FieldWidth - 1)
                         x++;
                     break;
+
                 case ConsoleKey.UpArrow:
                     if (y > 0)
                         y--;
                     break;
 
                 case ConsoleKey.DownArrow:
-                    if (y < fieldHeight - 1)
+                    if (y < FieldHeight - 1)
                         y++;
                     break;
+
                 case ConsoleKey.Spacebar:
-                field[x,y]=!field [x,y];
-                        
+                    field[x, y] = !field[x, y];
+                    break;
+
+                case ConsoleKey.N:
+                    {
+                        var newField = new bool[FieldWidth, FieldHeight];
+                        for (int row = 0; row < FieldHeight; row++)
+                        {
+                            for (int column = 0; column < FieldWidth; column++)
+                            {
+                                var count = 0;
+                                if (row > 0 && column > 0 && field[column - 1, row - 1])
+                                {
+                                    count++;
+                                }
+
+                                if (row > 0 && field[column, row - 1])
+                                {
+                                    count++;
+                                }
+
+                                if (row > 0 && column < FieldWidth - 1 && field[column + 1, row - 1])
+                                {
+                                    count++;
+                                }
+
+                                if (column < FieldWidth - 1 && field[column + 1, row])
+                                {
+                                    count++;
+                                }
+
+                                if (column < FieldWidth - 1 && row < FieldHeight - 1 && field[column + 1, row + 1])
+                                {
+                                    count++;
+                                }
+
+                                if (row < FieldHeight - 1 && field[column, row + 1])
+                                {
+                                    count++;
+                                }
+
+                                if (row < FieldHeight - 1 && column > 0 && field[column - 1, row + 1])
+                                {
+                                    count++;
+                                }
+
+                                if (column > 0 && field[column - 1, row])
+                                {
+                                    count++;
+                                }
+
+                                if (field[column, row])
+                                {
+                                    if (count == 2 || count == 3)
+                                    {
+                                        newField[column, row] = true;
+                                    }
+                                }
+                                else
+                                {
+                                    if (count == 3)
+                                    {
+                                        newField[column, row] = true;
+                                    }
+                                }
+                            }
+                        }
+
+                        field = newField;
+                    }
                     break;
             }
         }
